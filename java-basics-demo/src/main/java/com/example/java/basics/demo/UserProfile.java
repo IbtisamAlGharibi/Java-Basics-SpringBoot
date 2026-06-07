3ad8282a38338a4dd884d4aa3d6d11c8b8b8640e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 12:15:59 +0400

    getter and setter is generated

--- java-basics-demo/src/main/java/com/example/java/basics/demo/UserProfile.java
+++ java-basics-demo/src/main/java/com/example/java/basics/demo/UserProfile.java
@@ -10,4 +10,28 @@ public class UserProfile {
         this.phoneNumber = phoneNumber;
         this.address = address;
     }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
