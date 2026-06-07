13d84b7a1e096c79cf2a0c428babc06663da8018
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 11:16:27 +0400

    updateStudent() method is created

--- java-basics-demo/src/main/java/com/example/java/basics/demo/StudentController.java
+++ java-basics-demo/src/main/java/com/example/java/basics/demo/StudentController.java
@@ -1,12 +1,19 @@
 package com.example.java.basics.demo;
 
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
     Student student = new Student(" 101","A", " Ahmed ");
 
+    @PutMapping("/updateStudent")
+    public String updateStudent(@RequestParam String newStudentName){
+        String currentName = student.getStudentName();
+        student.setStudentName(newStudentName);
 
+        return  "Previous Name: " + currentName+ "New Name: " + student.getStudentName();
+    }
 
 }
