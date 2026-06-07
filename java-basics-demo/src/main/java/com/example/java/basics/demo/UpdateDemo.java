ce1526571495ac7668eaa77764022e058fe91293
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 09:44:48 +0400

    student name is displayed

--- java-basics-demo/src/main/java/com/example/java/basics/demo/UpdateDemo.java
+++ java-basics-demo/src/main/java/com/example/java/basics/demo/UpdateDemo.java
@@ -4,6 +4,9 @@ public class UpdateDemo {
     public static void main(String[] args){
         String currentName = "Ebtisam";
         String newName = "Ibtisam";
+        boolean status = false;
+
+        System.out.println("Current student name: " + currentName);
 
     }
 }
