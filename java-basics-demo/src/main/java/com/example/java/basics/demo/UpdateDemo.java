7c08fdcfe33336f861e41d16c0ee83db606c2a10
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 09:43:00 +0400

    make some changes in the class

--- java-basics-demo/src/main/java/com/example/java/basics/demo/UpdateDemo.java
+++ java-basics-demo/src/main/java/com/example/java/basics/demo/UpdateDemo.java
@@ -1,37 +1,9 @@
 package com.example.java.basics.demo;
 
 public class UpdateDemo {
-    String currentStudent;
-    String newStudent;
-    String status;
+    public static void main(String[] args){
+        String currentName = "Ebtisam";
+        String newName = "Ibtisam";
 
-    public UpdateDemo(String currentStudent, String newStudent, String status) {
-        this.currentStudent = currentStudent;
-        this.newStudent = newStudent;
-        this.status = status;
-    }
-
-    public String getCurrentStudent() {
-        return currentStudent;
-    }
-
-    public void setCurrentStudent(String currentStudent) {
-        this.currentStudent = currentStudent;
-    }
-
-    public String getNewStudent() {
-        return newStudent;
-    }
-
-    public void setNewStudent(String newStudent) {
-        this.newStudent = newStudent;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 }
