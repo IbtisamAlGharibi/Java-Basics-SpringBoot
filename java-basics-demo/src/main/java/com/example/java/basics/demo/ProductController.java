31bc88ad61c30d85c5c2170212613acf850c824e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 14:36:26 +0400

    new product object is created and entered in the hashmap

--- java-basics-demo/src/main/java/com/example/java/basics/demo/ProductController.java
+++ java-basics-demo/src/main/java/com/example/java/basics/demo/ProductController.java
@@ -1,10 +1,17 @@
 package com.example.java.basics.demo;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
 @RestController
 public class ProductController {
-    HashMap<Integer, Product> productHashMap = new HashMap<>();
+    static HashMap<Integer, Product> productHashMap = new HashMap<>();
+    Product product = new Product("1","Laptop",10);
+    public ProductController() {
+        productHashMap.put(1, product);
+    }
 }
