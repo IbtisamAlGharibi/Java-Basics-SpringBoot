5a8baa923b35d57365675bba17159e4e64de1d4a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 10:37:10 +0400

    displayInfo method is created

--- java-basics-demo/src/main/java/com/example/java/basics/demo/StudentController.java
+++ java-basics-demo/src/main/java/com/example/java/basics/demo/StudentController.java
@@ -3,4 +3,10 @@ package com.example.java.basics.demo;
 public class StudentController {
     Student student = new Student(" 101","A", " Ahmed ");
 
+    public void displayInfo(){
+        System.out.println("Student ID: " + student.getStudentID());
+        System.out.println("Student Name: " +student.getStudentName());
+        System.out.println("Student Grade: " + student.getGrade());
+    }
+
 }
