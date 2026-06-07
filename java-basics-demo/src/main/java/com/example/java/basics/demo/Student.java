575a69654aeaf4dab39adfdddf9b2826a2f76c25
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-07 09:59:48 +0400

    getter and setter argenerated

--- java-basics-demo/src/main/java/com/example/java/basics/demo/Student.java
+++ java-basics-demo/src/main/java/com/example/java/basics/demo/Student.java
@@ -10,4 +10,28 @@ public class Student {
         this.grade = grade;
         this.studentName = studentName;
     }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
 }
